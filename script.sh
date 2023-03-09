6e41ff14e7976c78669faa0922ca19b72e2db910
JoaoLCordeiro <joaolucascordeiro@gmail.com>
2023-03-09 14:54:52 -0300

    add hostfiles

--- script.sh
+++ script.sh
@@ -8,12 +8,24 @@ rm resultados.csv 2> /dev/null
 NMSG=5000
 
 # para bloqueantes
-for ARGTAM in 8 1024 4096 16384
+for ARGTAM in 16384
 do
-	echo "Mensagens de tamanho $ARGTAM" >> resultados.csv
+	echo "Mensagens de tamanho $ARGTAM com 8 processos" >> resultados.csv
 	for vez in $(seq 1 10)
 	do
-		mpirun -np 8 ./meuBcast "$NMSG" "$ARGTAM" -r 0 | grep 'Tempo:' | awk '{printf "%s\n",$2}' >> resultados.csv
+		mpirun -np 8 --hostfile hostfile8.txt ./meuBcast "$NMSG" "$ARGTAM" -r 0 | grep 'Tempo:' | awk '{printf "%s\n",$2}' >> resultados.csv
+	done
+
+	echo "Mensagens de tamanho $ARGTAM com 16 processos" >> resultados.csv
+	for vez in $(seq 1 10)
+	do
+		mpirun -np 16 --hostfile hostfile16.txt ./meuBcast "$NMSG" "$ARGTAM" -r 0 | grep 'Tempo:' | awk '{printf "%s\n",$2}' >> resultados.csv
+	done
+
+	echo "Mensagens de tamanho $ARGTAM com 32 processos" >> resultados.csv
+	for vez in $(seq 1 10)
+	do
+		mpirun -np 32 --hostfile hostfile32.txt ./meuBcast "$NMSG" "$ARGTAM" -r 0 | grep 'Tempo:' | awk '{printf "%s\n",$2}' >> resultados.csv
 	done
 	NMSG=$(expr $NMSG / 2)
 done
